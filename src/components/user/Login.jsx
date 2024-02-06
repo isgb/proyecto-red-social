a93423bde86d0d6cd730802fad5c1d55be47b2c8
isaias_pruebas <isgb_codigo@hotmail.com>
2024-02-05 20:11:25 -0600

    debug

--- src/components/user/Login.jsx
+++ src/components/user/Login.jsx
@@ -1,6 +1,18 @@
 import React from 'react'
+import { useForm } from '../../hooks/useForm'
 
 export const Login = () => {
+
+  const {form,changed} = useForm({});
+
+  const loginUser = async (e) => {
+    e.preventDefault();
+
+    console.log(form);
+
+    let userToLogin = form;
+  }
+
   return (
     <>
     <header className="content__header content__header--public">
@@ -9,16 +21,16 @@ export const Login = () => {
 
     <div className="content__posts">
 
-      <form className='form-login'>
+      <form className='form-login' onSubmit={loginUser}>
 
         <div className='form-group'>
           <label htmlFor='email'>Email</label>
-          <input type='email' name='email'/>
+          <input type='email' name='email' onChange={changed}/>
         </div>
 
         <div className='form-group'>
           <label htmlFor='password'>Contraseña</label>
-          <input type='password' name='password'/>
+          <input type='password' name='password' onChange={changed}/>
         </div>
 
         <input type='submit' value='Identificate' className='btn btn-success'/>
