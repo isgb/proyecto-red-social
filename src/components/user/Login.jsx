3e0e8c1db26a6c1eedb9e1be4b83ce466fa2eb4d
isaias_pruebas <isgb_codigo@hotmail.com>
2024-02-05 20:25:29 -0600

    debug

--- src/components/user/Login.jsx
+++ src/components/user/Login.jsx
@@ -1,43 +1,82 @@
-import React from 'react'
-import { useForm } from '../../hooks/useForm'
+import React, { useState } from "react";
+import { useForm } from "../../hooks/useForm";
+import { Global } from "../../helpers/Global";
 
 export const Login = () => {
-
-  const {form,changed} = useForm({});
+  const { form, changed } = useForm({});
+  const [ saved, setSaved ] = useState("not_sended");
 
   const loginUser = async (e) => {
     e.preventDefault();
 
-    console.log(form);
+    // console.log(form);
 
     let userToLogin = form;
-  }
+
+    const request = await fetch(Global.url + "user/login", {
+      method: "POST",
+      body: JSON.stringify(userToLogin),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await request.json();
+
+    console.log(data.user);
+    if (data.status == "success") {
+
+      //Persistir los datos en el navegador
+      localStorage.setItem("token", data.token);
+      localStorage.setItem("user",JSON.stringify(data.user));
+
+      setSaved("login");
+    } else {
+      setSaved("error");
+    }
+  };
 
   return (
     <>
-    <header className="content__header content__header--public">
-      <h1 className="content__title">Login</h1>
-    </header>
+      <header className="content__header content__header--public">
+        <h1 className="content__title">Login</h1>
+      </header>
 
-    <div className="content__posts">
+      <div className="content__posts">
+        {saved == "login" ? (
+          <strong className="alert alert-success">
+            Usuario identificado correctamente
+          </strong>
+        ) : (
+          ""
+        )}
 
-      <form className='form-login' onSubmit={loginUser}>
+        {saved == "error" ? (
+          <strong className="alert alert-danger">
+            El usuario no se ha registrado
+          </strong>
+        ) : (
+          ""
+        )}
 
-        <div className='form-group'>
-          <label htmlFor='email'>Email</label>
-          <input type='email' name='email' onChange={changed}/>
-        </div>
+        <form className="form-login" onSubmit={loginUser}>
+          <div className="form-group">
+            <label htmlFor="email">Email</label>
+            <input type="email" name="email" onChange={changed} />
+          </div>
 
-        <div className='form-group'>
-          <label htmlFor='password'>Contraseña</label>
-          <input type='password' name='password' onChange={changed}/>
-        </div>
+          <div className="form-group">
+            <label htmlFor="password">Contraseña</label>
+            <input type="password" name="password" onChange={changed} />
+          </div>
 
-        <input type='submit' value='Identificate' className='btn btn-success'/>
-
-      </form>
-
-    </div>
-  </>
-  )
-}
+          <input
+            type="submit"
+            value="Identificate"
+            className="btn btn-success"
+          />
+        </form>
+      </div>
+    </>
+  );
+};
