f658c8ce0dc720eb4493514e153451ac3f8fa06a
isaias_pruebas <isgb_codigo@hotmail.com>
2024-02-05 19:40:15 -0600

    debug

--- src/components/user/Register.jsx
+++ src/components/user/Register.jsx
@@ -1,18 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "../../hooks/useForm";
+import { Global } from "../../helpers/Global";
 
 export const Register = () => {
+  const { form, changed } = useForm({});
+  const [ saved, setSaved ] = useState("not_sended");
 
-  const {form,changed} = useForm({});
-
-  const saveUser = (e) => {
+  const saveUser = async (e) => {
     e.preventDefault();
 
     let newUser = form;
 
-    console.log(newUser)
+    // console.log(newUser)
+    const request = await fetch(Global.url + "user/register", {
+      method: "POST",
+      body: JSON.stringify(newUser),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
 
-  }
+    const data = await request.json();
+
+    // console.log(data)
+    if (data.status == "success") {
+      setSaved("saved");
+    } else {
+      setSaved("error");
+    }
+  };
 
   return (
     <>
@@ -21,36 +37,38 @@ export const Register = () => {
       </header>
 
       <div className="content__posts">
-          <form className="register-form" onSubmit={saveUser}>
 
-              <div className="form-group">
-                  <label htmlFor="name">Nombre</label>
-                  <input type="text" name="name" onChange={changed}/>
-              </div>
+        <strong className="alert alert-success">{saved == "saved" ? "Usuario registrado correctamente" : ''}</strong>
+        <strong className="alert alert-danger">{saved == "error" ? "El usuario no se ha registrado" : ''}</strong>
 
-              <div className="form-group">
-                  <label htmlFor="surname">Apellidos</label>
-                  <input type="text" name="surname" onChange={changed}/>
-              </div>
+        <form className="register-form" onSubmit={saveUser}>
+          <div className="form-group">
+            <label htmlFor="name">Nombre</label>
+            <input type="text" name="name" onChange={changed} />
+          </div>
 
-              <div className="form-group">
-                  <label htmlFor="nick">Nick</label>
-                  <input type="text" name="nick" onChange={changed}/>
-              </div>
+          <div className="form-group">
+            <label htmlFor="surname">Apellidos</label>
+            <input type="text" name="surname" onChange={changed} />
+          </div>
 
-              <div className="form-group">
-                  <label htmlFor="email">Correo Electrónico</label>
-                  <input type="email" name="email" onChange={changed}/>
-              </div>
+          <div className="form-group">
+            <label htmlFor="nick">Nick</label>
+            <input type="text" name="nick" onChange={changed} />
+          </div>
 
-              <div className="form-group">
-                  <label htmlFor="password">Nombre</label>
-                  <input type="password" name="password" onChange={changed}/>
-              </div>
+          <div className="form-group">
+            <label htmlFor="email">Correo Electrónico</label>
+            <input type="email" name="email" onChange={changed} />
+          </div>
 
-              <input type="submit" value="Registro" className="btn btn-success"/>
+          <div className="form-group">
+            <label htmlFor="password">Nombre</label>
+            <input type="password" name="password" onChange={changed} />
+          </div>
 
-          </form>
+          <input type="submit" value="Registro" className="btn btn-success" />
+        </form>
       </div>
     </>
   );
