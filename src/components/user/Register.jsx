cfc1ca9a50c31b148b69230b544a348227d02331
isaias_pruebas <isgb_codigo@hotmail.com>
2024-02-05 19:08:00 -0600

    debug

--- src/components/user/Register.jsx
+++ src/components/user/Register.jsx
@@ -8,7 +8,36 @@ export const Register = () => {
       </header>
 
       <div className="content__posts">
+          <form className="register-form">
 
+              <div className="form-group">
+                  <label htmlFor="name">Nombre</label>
+                  <input type="text" name="name"/>
+              </div>
+
+              <div className="form-group">
+                  <label htmlFor="surname">Apellidos</label>
+                  <input type="text" name="surname"/>
+              </div>
+
+              <div className="form-group">
+                  <label htmlFor="nick">Nick</label>
+                  <input type="text" name="nick"/>
+              </div>
+
+              <div className="form-group">
+                  <label htmlFor="email">Correo Electrónico</label>
+                  <input type="email" name="email"/>
+              </div>
+
+              <div className="form-group">
+                  <label htmlFor="password">Nombre</label>
+                  <input type="password" name="password"/>
+              </div>
+
+              <input type="submit" value="Registro" className="btn btn-success"/>
+
+          </form>
       </div>
     </>
   );
