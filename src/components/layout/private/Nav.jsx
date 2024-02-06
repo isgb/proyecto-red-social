6e689ecac2f04d6db07d944286425faaf7657e35
oscar <oscar.vasquez@zentro.com.mx>
2024-02-06 17:22:10 -0600

    debug

--- src/components/layout/private/Nav.jsx
+++ src/components/layout/private/Nav.jsx
@@ -7,24 +7,24 @@ export const Nav = () => {
     <nav className="navbar__container-lists">
     <ul className="container-lists__menu-list">
       <li className="menu-list__item">
-        <a href="#" className="menu-list__link">
+        <NavLink to="/social" className="menu-list__link">
           <i className="fa-solid fa-house"></i>
           <span className="menu-list__title">Inicio</span>
-        </a>
+        </NavLink>
       </li>
 
       <li className="menu-list__item">
-        <a href="#" className="menu-list__link">
+        <NavLink to="/social/feed" className="menu-list__link">
           <i className="fa-solid fa-list"></i>
           <span className="menu-list__title">Timeline</span>
-        </a>
+        </NavLink>
       </li>
 
       <li className="menu-list__item">
-        <a href="#" className="menu-list__link">
+        <NavLink to="/social/gente"className="menu-list__link">
           <i className="fa-solid fa-user"></i>
           <span className="menu-list__title">Gente</span>
-        </a>
+        </NavLink>
       </li>
 
    
@@ -46,10 +46,10 @@ export const Nav = () => {
         </a>
       </li>
       <li className="list-end__item">
-        <a href="#" className="list-end__link">
+        <NavLink to="/social/ajustes" className="list-end__link">
             <i className='fa-solid fa-gear'></i>
           <span className="list-end__name">Ajustes</span>
-        </a>
+        </NavLink>
       </li>
       <li className="list-end__item">
         <NavLink to="/social/logout" className="list-end__link">
--- src/router/Routing.jsx
+++ src/router/Routing.jsx
@@ -4,9 +4,11 @@ import { PublicLayout } from "../components/layout/public/PublicLayout";
 import { Login } from "../components/user/Login";
 import { Register } from "../components/user/Register";
 import { PrivateLayout } from "../components/layout/private/PrivateLayout";
-import { Feed } from "../components/publication/feed";
 import { AuthProvider } from "../context/AuthProvider";
 import { Logout } from "../components/user/Logout";
+import { Feed } from "../components/publication/Feed";
+import { People } from "../components/user/People";
+import { Config } from "../components/user/Config";
 
 export const Routing = () => {
   return (
@@ -21,8 +23,10 @@ export const Routing = () => {
 
           <Route path="/social" element={<PrivateLayout />}>
             <Route index element={<Feed />} />
-            <Route path="feed" element={<Feed />} />
+            <Route path="feed" element={<Feed/>} />
             <Route path="logout" element={<Logout />} />
+            <Route path="gente" element={<People />} />
+            <Route path="ajustes" element={<Config />} />
           </Route>
 
           <Route
