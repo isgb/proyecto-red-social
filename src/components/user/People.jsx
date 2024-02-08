b1994f308af60c3b6d8c963b87433ae88449becf
oscar <oscar.vasquez@zentro.com.mx>
2024-02-08 15:54:28 -0600

    debug

--- src/components/user/People.jsx
+++ src/components/user/People.jsx
@@ -1,11 +1,8 @@
 import React, { useEffect, useState } from 'react'
-import avatar from '../../assets/img/user.png'
 import { Global } from '../../helpers/Global'
-import useAuth from '../../hooks/useAuth'
+import { UserList } from './UserList'
 
 export const People = () => {
-
-  const { auth } = useAuth();
 
   const [users, setUsers] = useState([])
   const [page, setPage] = useState(1)
@@ -57,36 +54,6 @@ export const People = () => {
 
   }
 
-  const nextPage = () => {
-    let next = page + 1;
-    setPage(next);
-
-    getUsers(next);
-    console.log(following)
-    // console.log(page, users)
-  }
-
-  const follow = async (userId) => {
-    //peticion al backend para guardar el follow
-    const request = await fetch(Global.url + "follow/save", {
-      method: "POST",
-      body: JSON.stringify({ followed: userId }),
-      headers: {
-        "Content-Type": "application/json",
-        "Authorization": localStorage.getItem("token")
-      }
-    })
-
-    const data = await request.json();
-
-    //cuando este todi correcto
-    if (data.status == "success") {
-
-      // actualiza estado de following, agregando el nuevo follow
-      setFollowing([...following, userId])
-
-    }
-  }
 
   const unfollow = async (userId) => {
     //peticion al backend para borrar el follow
@@ -119,77 +86,16 @@ export const People = () => {
         <h1 className="content__title">Gente</h1>
       </header>
 
-      <div className="content__posts">
+      <UserList users={users} 
+                getUsers={getUsers}
+                following={following}
+                setFollowing={setFollowing}
+                page={page}
+                setPage={setPage}
+                more={more}
+                loading={loading}
+      />
 
-        {
-          users.map(user => {
-            return (
-
-              <article className="posts__post" key={user._id}>
-                <div className="post__container">
-                  <div className="post__image-user">
-
-                    <a href="#" className="post__image-link">
-                      {user.image != "default.png" && <img src={Global.url + "user/avatar/" + user.image} className="post__user-image" alt="Foto de perfil" />}
-                      {user.image == "default.png" && <img src={avatar} className="post__user-image" alt="Foto de perfil" />}
-                    </a>
-                  </div>
-
-                  <div className="post__body">
-                    <div className="post__user-info">
-                      <a href="#" className="user-info__name">
-                        {user.name} {user.surname}
-                      </a>
-                      <span className="user-info__divider"> | </span>
-                      <a href="#" className="user-info__create-date">
-                        {user.created_at}
-                      </a>
-                    </div>
-
-                    <h4 className="post__content">{user.bio}</h4>
-                  </div>
-                </div>
-
-                {user._id !== auth._id &&
-                  <div className="post__buttons">
-
-                    {!following.includes(user._id) &&
-                      <button className="post__button post__button--green"
-                        onClick={() => follow(user._id)}
-                      >
-                        Seguir
-                      </button>
-                    }
-
-                    {
-                      following.includes(user._id) &&
-                      <button className="post__button"
-                        onClick={() => unfollow(user._id)}
-                      >
-                        Dejar de Seguir
-                      </button>
-                    }
-
-                  </div>
-                }
-
-              </article>
-            )
-          })
-        }
-
-      </div>
-
-      {loading ? <div>Cargando...</div> : ""}
-
-      {
-        more &&
-        <div className="content__container-btn">
-          <button className="content__btn-more-post" onClick={nextPage}>
-            Ver mas personas
-          </button>
-        </div>
-      }
 
       <br />
 
